29df7f43dd9410e591a598f3f6898607ef2d3234
Riley Brown <brown.riley.c@gmail.com>
2019-04-02 18:07:02 -0400

    Updates

---
+++ precache-manifest.233700a85fc417afd48691e78214a7f7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b443b5a8c2907e0acdc",
+    "url": "/react-clicker/static/js/runtime~main.7dbc4b94.js"
+  },
+  {
+    "revision": "9ff0eb654fc2b93d34cd",
+    "url": "/react-clicker/static/js/main.3a3301c8.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/react-clicker/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "9ff0eb654fc2b93d34cd",
+    "url": "/react-clicker/static/css/main.2e1869d6.chunk.css"
+  },
+  {
+    "revision": "9809a86e8bb8e2949c174d63bb16fab9",
+    "url": "/react-clicker/index.html"
+  }
+];
